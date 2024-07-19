3e58d7a3363d8c780cf6279141e76aa60f2e933e
Conor Lee <your emailconorlee8@gmail.com>
2024-07-19 11:50:57 -0500

    big fix with added work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,134 +1,103 @@
-const dexPhoto=document.getElementById('dexPhoto');
-const search=document.querySelector('form');
-const input=document.getElementById('search');
+const dexPhoto = document.getElementById('dexPhoto');
+const search = document.querySelector('form');
+const input = document.getElementById('search');
 const hpText = document.getElementById('hpText');
 const atkText = document.getElementById('atkText');
 const spatkText = document.getElementById('spatkText');
 const defText = document.getElementById('defText');
 const spdefText = document.getElementById('spdefText');
 const speedText = document.getElementById('speedText');
-const Index = document.getElementById('index')
-const entry = document.getElementById('pokemon-entry')
-const pkmnName = document.getElementById('pokemon-name')
+const Index = document.getElementById('index');
+const entry = document.getElementById('pokemon-entry');
+const pkmnName = document.getElementById('pokemon-name');
+
 // Changes all pokemon information
 const renderPKMNInfo = function (event) {
- 
     event.preventDefault();
 
-const pokemon=input.value;
-const pokeURL=`https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+    const pokemon = input.value.toLowerCase();
+    const pokeURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
 
+    // Display pokemon image based on input
+    const fetchImage = function () {
+        fetch(pokeURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const pokeObjIMG = data;
+                const img = pokeObjIMG.sprites.other.home.front_default;
+                dexPhoto.setAttribute('src', img);
+            });
+    };
 
-// Display pokemon image based on input
-const fetchImage = function() {
-    
-    fetch(pokeURL)
-    .then(function(response){    
-    return response.json();
-    })
+    // Display Stats and adjust bars length accordingly
+    const fetchStats = function () {
+        const empty = [];
 
-    .then(function(data){
-        const pokeObjIMG=data; 
-        console.log(pokeObjIMG);
-        const img = pokeObjIMG.sprites.other.home.front_default;
-        console.log(img);
-        dexPhoto.setAttribute('src', img);
-         })  
-    }
+        fetch(pokeURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const pokeObjStat = data.stats;
 
-// Display Stats and adjust bars length accordingly
-const fetchStats = function () {
-   const empty = [];
+                for (let i = 0; i < pokeObjStat.length; i++) {
+                    const stats = pokeObjStat[i].base_stat;
+                    empty.push(stats);
+                }
 
-    fetch(pokeURL)
-    .then(function(response){    
-    return response.json();
-    })
+                hpText.textContent = empty[0];
+                atkText.textContent = empty[1];
+                defText.textContent = empty[2];
+                spatkText.textContent = empty[3];
+                spdefText.textContent = empty[4];
+                speedText.textContent = empty[5];
 
-    .then(function(data){
-        const pokeObjStat=data.stats; 
-        console.log(pokeObjStat);
+                const hpbar = document.getElementById('hpbar');
+                const atkbar = document.getElementById('atkbar');
+                const defbar = document.getElementById('defbar');
+                const spatkbar = document.getElementById('spatkbar');
+                const spdefbar = document.getElementById('spdefbar');
+                const speedbar = document.getElementById('speedbar');
 
-        for (let i=0;i<pokeObjStat.length;i++){
+                hpbar.style.width = `${((empty[0] / 255) * 100)}%`;
+                atkbar.style.width = `${((empty[1] / 255) * 100)}%`;
+                defbar.style.width = `${((empty[2] / 255) * 100)}%`;
+                spatkbar.style.width = `${((empty[3] / 255) * 100)}%`;
+                spdefbar.style.width = `${((empty[4] / 255) * 100)}%`;
+                speedbar.style.width = `${((empty[5] / 255) * 100)}%`;
+            });
+    };
 
-        const stats = pokeObjStat[i].base_stat;
-        
-            empty.push(stats); 
-        } 
-        
-        console.log(empty);
-        hpText.textContent = empty[0]
-        atkText.textContent = empty[1]
-        defText.textContent = empty[2]
-        spatkText.textContent = empty[3]
-        spdefText.textContent = empty[4]
-        speedText.textContent = empty[5]
+    const fetchBio = function () {
+        const bioURL = `https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`;
 
-        const hpbar = document.getElementById('hpbar')
-        const atkbar = document.getElementById('atkbar')
-        const defbar = document.getElementById('defbar')
-        const spatkbar = document.getElementById('spatkbar')
-        const spdefbar = document.getElementById('spdefbar')
-        const speedbar = document.getElementById('speedbar')
+        // Pull Name, Number, Dex Entry
+        fetch(bioURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const pokeObjBio = data;
+                // Dex-Entry
+                // Find english text
+                const enText = (pokeObjBio.flavor_text_entries.findIndex(obj => obj.language.name === 'en'));
+                // Updates Dex Entry and gets rid of unwanted characters
+                entry.innerHTML = `${pokeObjBio.flavor_text_entries[enText].flavor_text.replace('\f', " ")}`;
 
-        hpbar.style.width=`${((empty[0]/255)*100)}%`
-        atkbar.style.width=`${((empty[1]/255)*100)}%`
-        defbar.style.width=`${((empty[2]/255)*100)}%`
-        spatkbar.style.width=`${((empty[3]/255)*100)}%`
-        spdefbar.style.width=`${((empty[4]/255)*100)}%`
-        speedbar.style.width=`${((empty[5]/255)*100)}%`
+                // Dex Name + #
+                const zerofilled = ('0000' + pokeObjBio.id).slice(-4);
+                pkmnName.innerHTML = `${pokemon.charAt(0).toUpperCase() + pokemon.slice(1)}: #${zerofilled}`;
+            });
+    };
 
-        })   
-    }
-
-const fetchBio = function () {
-const bioURL =`https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`
-
-//Pull Name, Number, Dex Entry
-    fetch(bioURL)
-    .then(function(response){    
-    return response.json();
-    })
-    .then(function(data){
-        const pokeObjBio=data;
-        //Dex-Entry
-        //Find english text
-       const enText = (pokeObjBio.flavor_text_entries.findIndex(obj => obj.language.name === 'en'))
-        //Updates Dex Entry and gets rid of unwanted characters
-       entry.innerHTML=`${pokeObjBio.flavor_text_entries[enText].flavor_text.replace('\f'," ")}`;
-      
-      //Dex Name + #
-       const zerofilled = ('0000'+pokeObjBio.id).slice(-4);
-              pkmnName.innerHTML=`${pokemon.charAt(0).toUpperCase()
-                + pokemon.slice(1)}: #${zerofilled}` 
-
-    })}
-/*
-const fetchEvo = function {
-    fetch()
-    .then(function(response){
-        return response.json();
-    })
-    
-    .then(function(data){
-
-    })
-}
-*/
-fetchImage();
-fetchStats();
-fetchBio();
+    fetchImage();
+    fetchStats();
+    fetchBio();
 }
 
-
-
-
-
-
-
-
-search.addEventListener('submit', renderPKMNInfo)
-
+search.addEventListener('submit', renderPKMNInfo);
 
 // Stat Bars
 
@@ -142,7 +111,7 @@ document.getElementById('close-alert').addEventListener('click', () => {
 // FUNCTION TO SEARCH INDEX
 const searchIndexContainer = document.getElementById('search-index-container');
 
-// FUNTION TO FETCH-DISPLAY POKEMON DETAILS
+// FUNCTION TO FETCH-DISPLAY POKEMON DETAILS
 const fetchAndDisplayPokemon = function (pokemon) {
     const pokeURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
 
@@ -154,9 +123,7 @@ const fetchAndDisplayPokemon = function (pokemon) {
             })
             .then(function (data) {
                 const pokeObjIMG = data;
-                console.log(pokeObjIMG);
                 const img = pokeObjIMG.sprites.other.home.front_default;
-                console.log(img);
                 dexPhoto.setAttribute('src', img);
             });
     };
@@ -170,14 +137,12 @@ const fetchAndDisplayPokemon = function (pokemon) {
             })
             .then(function (data) {
                 const pokeObjStat = data.stats;
-                console.log(pokeObjStat);
 
                 for (let i = 0; i < pokeObjStat.length; i++) {
                     const stats = pokeObjStat[i].base_stat;
                     empty.push(stats);
                 }
 
-                console.log(empty);
                 hpText.textContent = empty[0];
                 atkText.textContent = empty[1];
                 defText.textContent = empty[2];
@@ -213,10 +178,12 @@ const populateSearchIndex = function () {
         .then(data => {
             const pokeList = data.results;
 
-            pokeList.forEach(pokemon => {
+            pokeList.forEach((pokemon, index) => {
                 const pokeElement = document.createElement('div');
-                pokeElement.textContent = pokemon.name;
-                pokeElement.classList.add('pill-navbar-item');
+                const zerofilled = ('0000' + (index + 1)).slice(-4);
+                const capitalizedPokemonName = `${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}`;
+                pokeElement.innerHTML = `${capitalizedPokemonName}: #${zerofilled}`;
+                pokeElement.classList.add('box');
                 pokeElement.style.fontSize = '1rem';
                 pokeElement.style.cursor = 'pointer';
                 pokeElement.draggable = true;
@@ -226,7 +193,7 @@ const populateSearchIndex = function () {
                 pokeElement.addEventListener('click', () => {
                     fetchAndDisplayMainPokemon(pokemon.name);
 
-                    document.querySelectorAll('.main-aside .pill-navbar-item').forEach(item => {
+                    document.querySelectorAll('.main-aside .box').forEach(item => {
                         item.classList.remove('clicked');
                     });
 
@@ -240,7 +207,6 @@ const populateSearchIndex = function () {
 populateSearchIndex();
 
 // FUNCTION TO DRAG AND DROP
-
 function allowDrop(event) {
     event.preventDefault();
 }
@@ -256,8 +222,8 @@ function drop(event) {
             const img = data.sprites.other.home.front_default;
             const imgElement = document.createElement('img');
             imgElement.src = img;
-            event.target.innerHTML = ''; // Clear any existing content
-            event.target.appendChild(imgElement); // Add the new image
+            event.target.innerHTML = '';
+            event.target.appendChild(imgElement);
         });
 }
 
@@ -265,7 +231,7 @@ function drop(event) {
 const clearPartyButton = document.getElementById('clear-party');
 clearPartyButton.addEventListener('click', () => {
     document.querySelectorAll('.party-slot').forEach(slot => {
-        slot.innerHTML = ''; // Clear all party slots
+        slot.innerHTML = '';
     });
 });
 
